1e733cca0a5b773e81111254146b51503d5d4e67
EagleOrangeRome <ruhulissl53@gmail.com>
2023-11-10 10:30:00 +0800

    feat: implement notification system with toast messages and context provider

---
+++ frontend/components/Notification.tsx
@@ -0,0 +1,348 @@
+import React, { createContext, useContext, useState, useCallback, useEffect } from 'react';
+
+export type NotificationType = 'success' | 'error' | 'warning' | 'info';
+
+export interface Notification {
+  id: string;
+  type: NotificationType;
+  title: string;
+  message?: string;
+  duration?: number;
+  persistent?: boolean;
+  action?: {
+    label: string;
+    onClick: () => void;
+  };
+}
+
+interface NotificationContextType {
+  notifications: Notification[];
+  addNotification: (notification: Omit<Notification, 'id'>) => void;
+  removeNotification: (id: string) => void;
+  clearNotifications: () => void;
+}
+
+const NotificationContext = createContext<NotificationContextType | undefined>(undefined);
+
+export const useNotification = () => {
+  const context = useContext(NotificationContext);
+  if (!context) {
+    throw new Error('useNotification must be used within a NotificationProvider');
+  }
+  return context;
+};
+
+interface NotificationProviderProps {
+  children: React.ReactNode;
+  maxNotifications?: number;
+}
+
+export const NotificationProvider: React.FC<NotificationProviderProps> = ({
+  children,
+  maxNotifications = 5,
+}) => {
+  const [notifications, setNotifications] = useState<Notification[]>([]);
+
+  const addNotification = useCallback((notification: Omit<Notification, 'id'>) => {
+    const id = `notification-${Date.now()}-${Math.random().toString(36).substr(2, 9)}`;
+    const newNotification: Notification = {
+      ...notification,
+      id,
+      duration: notification.duration ?? 5000,
+    };
+
+    setNotifications((prev) => {
+      const updated = [newNotification, ...prev];
+      return updated.slice(0, maxNotifications);
+    });
+  }, [maxNotifications]);
+
+  const removeNotification = useCallback((id: string) => {
+    setNotifications((prev) => prev.filter((notification) => notification.id !== id));
+  }, []);
+
+  const clearNotifications = useCallback(() => {
+    setNotifications([]);
+  }, []);
+
+  return (
+    <NotificationContext.Provider
+      value={{
+        notifications,
+        addNotification,
+        removeNotification,
+        clearNotifications,
+      }}
+    >
+      {children}
+      <NotificationContainer />
+    </NotificationContext.Provider>
+  );
+};
+
+const NotificationContainer: React.FC = () => {
+  const { notifications } = useNotification();
+
+  return (
+    <div className="fixed top-4 right-4 z-50 space-y-2">
+      {notifications.map((notification) => (
+        <NotificationItem key={notification.id} notification={notification} />
+      ))}
+    </div>
+  );
+};
+
+interface NotificationItemProps {
+  notification: Notification;
+}
+
+const NotificationItem: React.FC<NotificationItemProps> = ({ notification }) => {
+  const { removeNotification } = useNotification();
+  const [isVisible, setIsVisible] = useState(false);
+  const [isLeaving, setIsLeaving] = useState(false);
+
+  useEffect(() => {
+    // Animate in
+    const timer = setTimeout(() => setIsVisible(true), 10);
+    return () => clearTimeout(timer);
+  }, []);
+
+  useEffect(() => {
+    if (!notification.persistent && notification.duration && notification.duration > 0) {
+      const timer = setTimeout(() => {
+        handleClose();
+      }, notification.duration);
+      return () => clearTimeout(timer);
+    }
+  }, [notification.duration, notification.persistent]);
+
+  const handleClose = () => {
+    setIsLeaving(true);
+    setTimeout(() => {
+      removeNotification(notification.id);
+    }, 300); // Match animation duration
+  };
+
+  const getNotificationStyles = () => {
+    const baseStyles = `
+      transform transition-all duration-300 ease-in-out
+      ${isVisible && !isLeaving ? 'translate-x-0 opacity-100' : 'translate-x-full opacity-0'}
+      max-w-sm w-full bg-white dark:bg-gray-800 shadow-lg rounded-lg pointer-events-auto ring-1 ring-black ring-opacity-5 overflow-hidden
+    `;
+
+    const typeStyles = {
+      success: 'border-l-4 border-green-400',
+      error: 'border-l-4 border-red-400',
+      warning: 'border-l-4 border-yellow-400',
+      info: 'border-l-4 border-blue-400',
+    };
+
+    return `${baseStyles} ${typeStyles[notification.type]}`;
+  };
+
+  const getIcon = () => {
+    const iconClasses = 'w-5 h-5';
+    
+    switch (notification.type) {
+      case 'success':
+        return (
+          <svg className={`${iconClasses} text-green-400`} fill="none" stroke="currentColor" viewBox="0 0 24 24">
+            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
+          </svg>
+        );
+      case 'error':
+        return (
+          <svg className={`${iconClasses} text-red-400`} fill="none" stroke="currentColor" viewBox="0 0 24 24">
+            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 8v4m0 4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+          </svg>
+        );
+      case 'warning':
+        return (
+          <svg className={`${iconClasses} text-yellow-400`} fill="none" stroke="currentColor" viewBox="0 0 24 24">
+            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z" />
+          </svg>
+        );
+      case 'info':
+        return (
+          <svg className={`${iconClasses} text-blue-400`} fill="none" stroke="currentColor" viewBox="0 0 24 24">
+            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+          </svg>
+        );
+      default:
+        return null;
+    }
+  };
+
+  return (
+    <div className={getNotificationStyles()}>
+      <div className="p-4">
+        <div className="flex items-start">
+          <div className="flex-shrink-0">
+            {getIcon()}
+          </div>
+          <div className="ml-3 w-0 flex-1">
+            <p className="text-sm font-medium text-gray-900 dark:text-white">
+              {notification.title}
+            </p>
+            {notification.message && (
+              <p className="mt-1 text-sm text-gray-500 dark:text-gray-400">
+                {notification.message}
+              </p>
+            )}
+            {notification.action && (
+              <div className="mt-3">
+                <button
+                  onClick={notification.action.onClick}
+                  className="text-sm font-medium text-primary-600 dark:text-primary-400 hover:text-primary-500 dark:hover:text-primary-300"
+                >
+                  {notification.action.label}
+                </button>
+              </div>
+            )}
+          </div>
+          <div className="ml-4 flex-shrink-0 flex">
+            <button
+              onClick={handleClose}
+              className="bg-white dark:bg-gray-800 rounded-md inline-flex text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-primary-500"
+            >
+              <span className="sr-only">Close</span>
+              <svg className="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M6 18L18 6M6 6l12 12" />
+              </svg>
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+// Convenience hooks for different notification types
+export const useNotificationHelpers = () => {
+  const { addNotification } = useNotification();
+
+  const showSuccess = useCallback((title: string, message?: string, options?: Partial<Notification>) => {
+    addNotification({
+      type: 'success',
+      title,
+      message,
+      ...options,
+    });
+  }, [addNotification]);
+
+  const showError = useCallback((title: string, message?: string, options?: Partial<Notification>) => {
+    addNotification({
+      type: 'error',
+      title,
+      message,
+      persistent: true, // Errors should be persistent by default
+      ...options,
+    });
+  }, [addNotification]);
+
+  const showWarning = useCallback((title: string, message?: string, options?: Partial<Notification>) => {
+    addNotification({
+      type: 'warning',
+      title,
+      message,
+      ...options,
+    });
+  }, [addNotification]);
+
+  const showInfo = useCallback((title: string, message?: string, options?: Partial<Notification>) => {
+    addNotification({
+      type: 'info',
+      title,
+      message,
+      ...options,
+    });
+  }, [addNotification]);
+
+  return {
+    showSuccess,
+    showError,
+    showWarning,
+    showInfo,
+  };
+};
+
+// Toast component for simple notifications
+interface ToastProps {
+  type: NotificationType;
+  title: string;
+  message?: string;
+  onClose?: () => void;
+  className?: string;
+}
+
+export const Toast: React.FC<ToastProps> = ({
+  type,
+  title,
+  message,
+  onClose,
+  className = '',
+}) => {
+  const getToastStyles = () => {
+    const baseStyles = 'flex items-center p-4 rounded-lg shadow-lg';
+    const typeStyles = {
+      success: 'bg-green-50 dark:bg-green-900 text-green-800 dark:text-green-200',
+      error: 'bg-red-50 dark:bg-red-900 text-red-800 dark:text-red-200',
+      warning: 'bg-yellow-50 dark:bg-yellow-900 text-yellow-800 dark:text-yellow-200',
+      info: 'bg-blue-50 dark:bg-blue-900 text-blue-800 dark:text-blue-200',
+    };
+    return `${baseStyles} ${typeStyles[type]} ${className}`;
+  };
+
+  const getIcon = () => {
+    const iconClasses = 'w-5 h-5 mr-3';
+    
+    switch (type) {
+      case 'success':
+        return (
+          <svg className={`${iconClasses} text-green-400`} fill="currentColor" viewBox="0 0 20 20">
+            <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clipRule="evenodd" />
+          </svg>
+        );
+      case 'error':
+        return (
+          <svg className={`${iconClasses} text-red-400`} fill="currentColor" viewBox="0 0 20 20">
+            <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zM8.707 7.293a1 1 0 00-1.414 1.414L8.586 10l-1.293 1.293a1 1 0 101.414 1.414L10 11.414l1.293 1.293a1 1 0 001.414-1.414L11.414 10l1.293-1.293a1 1 0 00-1.414-1.414L10 8.586 8.707 7.293z" clipRule="evenodd" />
+          </svg>
+        );
+      case 'warning':
+        return (
+          <svg className={`${iconClasses} text-yellow-400`} fill="currentColor" viewBox="0 0 20 20">
+            <path fillRule="evenodd" d="M8.257 3.099c.765-1.36 2.722-1.36 3.486 0l5.58 9.92c.75 1.334-.213 2.98-1.742 2.98H4.42c-1.53 0-2.493-1.646-1.743-2.98l5.58-9.92zM11 13a1 1 0 11-2 0 1 1 0 012 0zm-1-8a1 1 0 00-1 1v3a1 1 0 002 0V6a1 1 0 00-1-1z" clipRule="evenodd" />
+          </svg>
+        );
+      case 'info':
+        return (
+          <svg className={`${iconClasses} text-blue-400`} fill="currentColor" viewBox="0 0 20 20">
+            <path fillRule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z" clipRule="evenodd" />
+          </svg>
+        );
+      default:
+        return null;
+    }
+  };
+
+  return (
+    <div className={getToastStyles()}>
+      {getIcon()}
+      <div className="flex-1">
+        <p className="font-medium">{title}</p>
+        {message && <p className="text-sm opacity-90">{message}</p>}
+      </div>
+      {onClose && (
+        <button
+          onClick={onClose}
+          className="ml-4 text-gray-400 hover:text-gray-600 dark:hover:text-gray-300"
+        >
+          <svg className="w-4 h-4" fill="currentColor" viewBox="0 0 20 20">
+            <path fillRule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clipRule="evenodd" />
+          </svg>
+        </button>
+      )}
+    </div>
+  );
+};
