b40a3e8cb7487307537750d6090d71e3e400b653
CamilleWinifred <ngadaominh187@gmail.com>
2023-11-10 09:00:00 +0800

    feat: create comprehensive form components with validation and file upload

---
+++ frontend/components/FormComponents.tsx
@@ -0,0 +1,482 @@
+import React, { forwardRef, InputHTMLAttributes, TextareaHTMLAttributes, SelectHTMLAttributes } from 'react';
+
+// Input Component
+interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
+  label?: string;
+  error?: string;
+  helperText?: string;
+  leftIcon?: React.ReactNode;
+  rightIcon?: React.ReactNode;
+}
+
+export const Input = forwardRef<HTMLInputElement, InputProps>(
+  ({ label, error, helperText, leftIcon, rightIcon, className = '', ...props }, ref) => {
+    return (
+      <div className="w-full">
+        {label && (
+          <label className="block text-sm font-medium text-gray-700 dark:text-gray-300 mb-2">
+            {label}
+            {props.required && <span className="text-red-500 ml-1">*</span>}
+          </label>
+        )}
+        
+        <div className="relative">
+          {leftIcon && (
+            <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
+              <div className="text-gray-400">{leftIcon}</div>
+            </div>
+          )}
+          
+          <input
+            ref={ref}
+            className={`
+              input-field
+              ${leftIcon ? 'pl-10' : ''}
+              ${rightIcon ? 'pr-10' : ''}
+              ${error ? 'border-red-300 focus:border-red-500 focus:ring-red-500' : ''}
+              ${className}
+            `}
+            {...props}
+          />
+          
+          {rightIcon && (
+            <div className="absolute inset-y-0 right-0 pr-3 flex items-center pointer-events-none">
+              <div className="text-gray-400">{rightIcon}</div>
+            </div>
+          )}
+        </div>
+        
+        {error && (
+          <p className="mt-1 text-sm text-red-600 dark:text-red-400">{error}</p>
+        )}
+        
+        {helperText && !error && (
+          <p className="mt-1 text-sm text-gray-500 dark:text-gray-400">{helperText}</p>
+        )}
+      </div>
+    );
+  }
+);
+
+Input.displayName = 'Input';
+
+// Textarea Component
+interface TextareaProps extends TextareaHTMLAttributes<HTMLTextAreaElement> {
+  label?: string;
+  error?: string;
+  helperText?: string;
+}
+
+export const Textarea = forwardRef<HTMLTextAreaElement, TextareaProps>(
+  ({ label, error, helperText, className = '', ...props }, ref) => {
+    return (
+      <div className="w-full">
+        {label && (
+          <label className="block text-sm font-medium text-gray-700 dark:text-gray-300 mb-2">
+            {label}
+            {props.required && <span className="text-red-500 ml-1">*</span>}
+          </label>
+        )}
+        
+        <textarea
+          ref={ref}
+          className={`
+            input-field
+            ${error ? 'border-red-300 focus:border-red-500 focus:ring-red-500' : ''}
+            ${className}
+          `}
+          {...props}
+        />
+        
+        {error && (
+          <p className="mt-1 text-sm text-red-600 dark:text-red-400">{error}</p>
+        )}
+        
+        {helperText && !error && (
+          <p className="mt-1 text-sm text-gray-500 dark:text-gray-400">{helperText}</p>
+        )}
+      </div>
+    );
+  }
+);
+
+Textarea.displayName = 'Textarea';
+
+// Select Component
+interface SelectProps extends SelectHTMLAttributes<HTMLSelectElement> {
+  label?: string;
+  error?: string;
+  helperText?: string;
+  options: { value: string; label: string; disabled?: boolean }[];
+  placeholder?: string;
+}
+
+export const Select = forwardRef<HTMLSelectElement, SelectProps>(
+  ({ label, error, helperText, options, placeholder, className = '', ...props }, ref) => {
+    return (
+      <div className="w-full">
+        {label && (
+          <label className="block text-sm font-medium text-gray-700 dark:text-gray-300 mb-2">
+            {label}
+            {props.required && <span className="text-red-500 ml-1">*</span>}
+          </label>
+        )}
+        
+        <select
+          ref={ref}
+          className={`
+            input-field
+            ${error ? 'border-red-300 focus:border-red-500 focus:ring-red-500' : ''}
+            ${className}
+          `}
+          {...props}
+        >
+          {placeholder && (
+            <option value="" disabled>
+              {placeholder}
+            </option>
+          )}
+          {options.map((option) => (
+            <option key={option.value} value={option.value} disabled={option.disabled}>
+              {option.label}
+            </option>
+          ))}
+        </select>
+        
+        {error && (
+          <p className="mt-1 text-sm text-red-600 dark:text-red-400">{error}</p>
+        )}
+        
+        {helperText && !error && (
+          <p className="mt-1 text-sm text-gray-500 dark:text-gray-400">{helperText}</p>
+        )}
+      </div>
+    );
+  }
+);
+
+Select.displayName = 'Select';
+
+// Checkbox Component
+interface CheckboxProps extends InputHTMLAttributes<HTMLInputElement> {
+  label?: string;
+  error?: string;
+  helperText?: string;
+}
+
+export const Checkbox = forwardRef<HTMLInputElement, CheckboxProps>(
+  ({ label, error, helperText, className = '', ...props }, ref) => {
+    return (
+      <div className="w-full">
+        <div className="flex items-center">
+          <input
+            ref={ref}
+            type="checkbox"
+            className={`
+              h-4 w-4 text-primary-600 focus:ring-primary-500 border-gray-300 rounded
+              ${error ? 'border-red-300' : ''}
+              ${className}
+            `}
+            {...props}
+          />
+          {label && (
+            <label className="ml-2 block text-sm text-gray-700 dark:text-gray-300">
+              {label}
+              {props.required && <span className="text-red-500 ml-1">*</span>}
+            </label>
+          )}
+        </div>
+        
+        {error && (
+          <p className="mt-1 text-sm text-red-600 dark:text-red-400">{error}</p>
+        )}
+        
+        {helperText && !error && (
+          <p className="mt-1 text-sm text-gray-500 dark:text-gray-400">{helperText}</p>
+        )}
+      </div>
+    );
+  }
+);
+
+Checkbox.displayName = 'Checkbox';
+
+// Radio Group Component
+interface RadioOption {
+  value: string;
+  label: string;
+  disabled?: boolean;
+}
+
+interface RadioGroupProps {
+  name: string;
+  options: RadioOption[];
+  value?: string;
+  onChange?: (value: string) => void;
+  label?: string;
+  error?: string;
+  helperText?: string;
+  direction?: 'horizontal' | 'vertical';
+}
+
+export const RadioGroup: React.FC<RadioGroupProps> = ({
+  name,
+  options,
+  value,
+  onChange,
+  label,
+  error,
+  helperText,
+  direction = 'vertical',
+}) => {
+  return (
+    <div className="w-full">
+      {label && (
+        <label className="block text-sm font-medium text-gray-700 dark:text-gray-300 mb-2">
+          {label}
+        </label>
+      )}
+      
+      <div className={`${direction === 'horizontal' ? 'flex space-x-4' : 'space-y-2'}`}>
+        {options.map((option) => (
+          <div key={option.value} className="flex items-center">
+            <input
+              type="radio"
+              name={name}
+              value={option.value}
+              checked={value === option.value}
+              onChange={(e) => onChange?.(e.target.value)}
+              disabled={option.disabled}
+              className={`
+                h-4 w-4 text-primary-600 focus:ring-primary-500 border-gray-300
+                ${error ? 'border-red-300' : ''}
+              `}
+            />
+            <label className="ml-2 block text-sm text-gray-700 dark:text-gray-300">
+              {option.label}
+            </label>
+          </div>
+        ))}
+      </div>
+      
+      {error && (
+        <p className="mt-1 text-sm text-red-600 dark:text-red-400">{error}</p>
+      )}
+      
+      {helperText && !error && (
+        <p className="mt-1 text-sm text-gray-500 dark:text-gray-400">{helperText}</p>
+      )}
+    </div>
+  );
+};
+
+// File Upload Component
+interface FileUploadProps {
+  label?: string;
+  error?: string;
+  helperText?: string;
+  accept?: string;
+  multiple?: boolean;
+  onChange?: (files: FileList | null) => void;
+  maxSize?: number; // in bytes
+  maxFiles?: number;
+}
+
+export const FileUpload: React.FC<FileUploadProps> = ({
+  label,
+  error,
+  helperText,
+  accept,
+  multiple = false,
+  onChange,
+  maxSize,
+  maxFiles,
+}) => {
+  const [dragOver, setDragOver] = React.useState(false);
+  const [files, setFiles] = React.useState<File[]>([]);
+  const fileInputRef = React.useRef<HTMLInputElement>(null);
+
+  const handleFiles = (fileList: FileList | null) => {
+    if (!fileList) return;
+
+    let newFiles = Array.from(fileList);
+
+    // Validate file size
+    if (maxSize) {
+      newFiles = newFiles.filter(file => {
+        if (file.size > maxSize) {
+          console.warn(`File ${file.name} exceeds maximum size of ${maxSize} bytes`);
+          return false;
+        }
+        return true;
+      });
+    }
+
+    // Validate number of files
+    if (maxFiles && newFiles.length > maxFiles) {
+      newFiles = newFiles.slice(0, maxFiles);
+    }
+
+    if (!multiple) {
+      newFiles = newFiles.slice(0, 1);
+    }
+
+    setFiles(newFiles);
+    onChange?.(fileList);
+  };
+
+  const handleDrop = (e: React.DragEvent) => {
+    e.preventDefault();
+    setDragOver(false);
+    handleFiles(e.dataTransfer.files);
+  };
+
+  const handleDragOver = (e: React.DragEvent) => {
+    e.preventDefault();
+    setDragOver(true);
+  };
+
+  const handleDragLeave = (e: React.DragEvent) => {
+    e.preventDefault();
+    setDragOver(false);
+  };
+
+  const removeFile = (index: number) => {
+    const newFiles = files.filter((_, i) => i !== index);
+    setFiles(newFiles);
+  };
+
+  return (
+    <div className="w-full">
+      {label && (
+        <label className="block text-sm font-medium text-gray-700 dark:text-gray-300 mb-2">
+          {label}
+        </label>
+      )}
+      
+      <div
+        className={`
+          border-2 border-dashed rounded-lg p-6 text-center cursor-pointer transition-colors
+          ${dragOver ? 'border-primary-400 bg-primary-50 dark:bg-primary-900/20' : 'border-gray-300 dark:border-gray-600'}
+          ${error ? 'border-red-300' : ''}
+          hover:border-primary-400 hover:bg-primary-50 dark:hover:bg-primary-900/20
+        `}
+        onDrop={handleDrop}
+        onDragOver={handleDragOver}
+        onDragLeave={handleDragLeave}
+        onClick={() => fileInputRef.current?.click()}
+      >
+        <input
+          ref={fileInputRef}
+          type="file"
+          accept={accept}
+          multiple={multiple}
+          onChange={(e) => handleFiles(e.target.files)}
+          className="hidden"
+        />
+        
+        <div className="space-y-2">
+          <svg
+            className="mx-auto h-12 w-12 text-gray-400"
+            stroke="currentColor"
+            fill="none"
+            viewBox="0 0 48 48"
+          >
+            <path
+              d="M28 8H12a4 4 0 00-4 4v20m32-12v8m0 0v8a4 4 0 01-4 4H12a4 4 0 01-4-4v-4m32-4l-3.172-3.172a4 4 0 00-5.656 0L28 28M8 32l9.172-9.172a4 4 0 015.656 0L28 28m0 0l4 4m4-24h8m-4-4v8m-12 4h.02"
+              strokeWidth={2}
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            />
+          </svg>
+          <div className="text-sm text-gray-600 dark:text-gray-400">
+            <span className="font-medium text-primary-600 dark:text-primary-400">
+              Click to upload
+            </span>{' '}
+            or drag and drop
+          </div>
+          {accept && (
+            <p className="text-xs text-gray-500 dark:text-gray-400">
+              {accept.split(',').join(', ')}
+            </p>
+          )}
+          {maxSize && (
+            <p className="text-xs text-gray-500 dark:text-gray-400">
+              Max size: {(maxSize / 1024 / 1024).toFixed(1)}MB
+            </p>
+          )}
+        </div>
+      </div>
+      
+      {files.length > 0 && (
+        <div className="mt-4 space-y-2">
+          {files.map((file, index) => (
+            <div
+              key={index}
+              className="flex items-center justify-between p-2 bg-gray-50 dark:bg-gray-800 rounded"
+            >
+              <div className="flex items-center space-x-2">
+                <svg className="w-4 h-4 text-gray-500" fill="currentColor" viewBox="0 0 20 20">
+                  <path d="M4 3a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V5a2 2 0 00-2-2H4zm12 12H4l4-8 3 6 2-4 3 6z" />
+                </svg>
+                <span className="text-sm text-gray-700 dark:text-gray-300">{file.name}</span>
+                <span className="text-xs text-gray-500">
+                  ({(file.size / 1024).toFixed(1)} KB)
+                </span>
+              </div>
+              <button
+                type="button"
+                onClick={(e) => {
+                  e.stopPropagation();
+                  removeFile(index);
+                }}
+                className="text-red-500 hover:text-red-700"
+              >
+                <svg className="w-4 h-4" fill="currentColor" viewBox="0 0 20 20">
+                  <path
+                    fillRule="evenodd"
+                    d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                    clipRule="evenodd"
+                  />
+                </svg>
+              </button>
+            </div>
+          ))}
+        </div>
+      )}
+      
+      {error && (
+        <p className="mt-1 text-sm text-red-600 dark:text-red-400">{error}</p>
+      )}
+      
+      {helperText && !error && (
+        <p className="mt-1 text-sm text-gray-500 dark:text-gray-400">{helperText}</p>
+      )}
+    </div>
+  );
+};
+
+// Form Container Component
+interface FormProps {
+  children: React.ReactNode;
+  onSubmit?: (e: React.FormEvent) => void;
+  className?: string;
+}
+
+export const Form: React.FC<FormProps> = ({ children, onSubmit, className = '' }) => {
+  return (
+    <form onSubmit={onSubmit} className={`space-y-6 ${className}`}>
+      {children}
+    </form>
+  );
+};
+
+// Form Group Component
+interface FormGroupProps {
+  children: React.ReactNode;
+  className?: string;
+}
+
+export const FormGroup: React.FC<FormGroupProps> = ({ children, className = '' }) => {
+  return <div className={`space-y-4 ${className}`}>{children}</div>;
+};
